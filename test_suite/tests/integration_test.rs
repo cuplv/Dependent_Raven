ce332089c7a3465779164b88c2291975def2261c
kylekim72 <kunhakim1206@gmail.com>
2026-06-08 13:28:44 -0600

    Commented out temporarily

--- test_suite/tests/integration_test.rs
+++ test_suite/tests/integration_test.rs
@@ -1,29 +1,29 @@
-#[ravencheck::module]
-#[declare_types(u32)]
-mod my_mod {
-    use std::collections::HashSet;
+// #[ravencheck::module]
+// #[declare_types(u32)]
+// mod my_mod {
+//     use std::collections::HashSet;
 
-    #[declare]
-    type MySet = HashSet<u32>;
+//     #[declare]
+//     type MySet = HashSet<u32>;
 
-    #[define]
-    #[derive(Clone, Debug, PartialEq, Eq)]
-    pub enum Nat {
-        Z,
-        S(Box<Nat>)
-    }
+//     #[define]
+//     #[derive(Clone, Debug, PartialEq, Eq)]
+//     pub enum Nat {
+//         Z,
+//         S(Box<Nat>)
+//     }
 
-    #[declare]
-    pub fn member(e: u32, s: MySet) -> bool {
-        s.contains(&e)
-    }
+//     #[declare]
+//     pub fn member(e: u32, s: MySet) -> bool {
+//         s.contains(&e)
+//     }
 
-    #[val((a: MySet, b: MySet) -> c: MySet { forall(|e: u32| member(e, c) == (member(e, a) || member(e, b))) })]
-    pub fn union(a: MySet, b: MySet) -> MySet {
-        a
-    }
+//     #[val((a: MySet, b: MySet) -> c: MySet { forall(|e: u32| member(e, c) == (member(e, a) || member(e, b))) })]
+//     pub fn union(a: MySet, b: MySet) -> MySet {
+//         a
+//     }
 
-    #[lemma((a: MySet, b: MySet) -> Lemma(union(union(a,b), b) == union(a,b)))]
-    fn union_idempotent(a: MySet, b: MySet) { 
-    }
-}
+//     #[lemma((a: MySet, b: MySet) -> Lemma(union(union(a,b), b) == union(a,b)))]
+//     fn union_idempotent(a: MySet, b: MySet) { 
+//     }
+// }
