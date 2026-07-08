fbdade31da31d0b32f3ca59728a1a1a59cc6c4e2
kylekim72 <kunhakim1206@gmail.com>
2026-07-07 18:37:15 -0600

    TIP prop #23

---
+++ test_suite/tests/prop_23.rs
@@ -0,0 +1,41 @@
+// TIP IsaPlanner prop_23: max(a, b) == max(b, a)
+#[ravencheck::module]
+mod tip_benchmarks {
+
+    #[define]
+    #[derive(Clone, Debug, PartialEq, Eq)]
+    pub enum Nat {
+        Z,
+        S(Box<Nat>),
+    }
+
+    #[val]
+    #[recursive]
+    fn max(x: Nat, y: Nat) -> Nat {
+        match x.clone() {
+            Nat::Z => y,
+            Nat::S(x_min) => match y {
+                Nat::Z => x,
+                Nat::S(y_min) => Nat::S(Box::new(max(*x_min, *y_min))),
+            },
+        }
+    }
+
+    #[val((a: Nat, b: Nat) -> Lemma(max(a, b) == max(b, a)))]
+    fn tip_23(a: Nat, b: Nat) {
+        match a {
+            Nat::Z => match b {
+                Nat::Z => (),
+                Nat::S(_b_min) => (),
+            },
+            Nat::S(a_min) => match b {
+                Nat::Z => (),
+                Nat::S(b_min) => {
+                    instantiate!(Nat::S(max(a_min, b_min)));
+                    instantiate!(Nat::S(max(b_min, a_min)));
+                    tip_23(*a_min, *b_min);
+                }
+            },
+        }
+    }
+}
