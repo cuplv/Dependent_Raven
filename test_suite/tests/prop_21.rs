7ed74f0f0e9449dd39921fc72b062f7145103589
kylekim72 <kunhakim1206@gmail.com>
2026-07-07 18:36:58 -0600

    TIP prop #21

---
+++ test_suite/tests/prop_21.rs
@@ -0,0 +1,44 @@
+// TIP IsaPlanner prop_21: le(n, add(n, m))  --  n <= n + m
+// Exercises a Bool-returning recursive function (le) used as a proposition.
+#[ravencheck::module]
+mod tip_benchmarks {
+
+    #[define]
+    #[derive(Clone, Debug, PartialEq, Eq)]
+    pub enum Nat {
+        Z,
+        S(Box<Nat>),
+    }
+
+    #[val]
+    #[recursive]
+    pub fn add(x: Nat, y: Nat) -> Nat {
+        match x {
+            Nat::Z => y,
+            Nat::S(x_min) => Nat::S(Box::new(add(*x_min, y))),
+        }
+    }
+
+    #[val]
+    #[recursive]
+    fn le(x: Nat, y: Nat) -> bool {
+        match x {
+            Nat::Z => true,
+            Nat::S(x_min) => match y {
+                Nat::Z => false,
+                Nat::S(y_min) => le(*x_min, *y_min),
+            },
+        }
+    }
+
+    #[val((n: Nat, m: Nat) -> Lemma(le(n, add(n, m))))]
+    fn tip_21(n: Nat, m: Nat) {
+        match n {
+            Nat::Z => (),
+            Nat::S(n_min) => {
+                instantiate!(Nat::S(add(n_min, m)));
+                tip_21(*n_min, m);
+            }
+        }
+    }
+}
